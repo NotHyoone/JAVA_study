824a4ab2e1c68fcfedf242c0999c6169e1fd2501
AHW <153744936+NotHyoone@users.noreply.github.com>
2025-04-19 13:29:59 +0900

    Create Ch01Person.java

---
+++ 코리아IT아카데미/Ch13Class/Ch01Person.java
@@ -0,0 +1,21 @@
+package Ch13Class;
+
+public class C01Person {
+	// 속성
+	String name;
+	int age;
+	String address;
+	
+	// 기능(함수, 메서드)
+	void hello() {
+		System.out.println(name + "이/가 인사합니다.");
+	}
+}
+
+
+
+
+
+
+
+
